e46c9875e023084162a72c1284627823b7dc8165
Dorffdaniel <danielcontabill@hotmail.com>
2025-04-04 21:07:04 +0100

    alteracao na profissao

--- src/js/script.js
+++ src/js/script.js
@@ -5,8 +5,12 @@ let valor = "Desenvolvedor Front-end";
 let index =0;
 
 function efeitoPalavra(){
-    areaProfissao.innerHTML= valor.substring(0, index);
+   let textoAtual = valor.substring(0, index)
 
+   let part1 = textoAtual.substring(0,13)
+   let part2 = textoAtual.substring(13)
+   
+   areaProfissao.innerHTML = `${part1} <span class='amarela'> ${part2}</span>`
     index++;
     if(index > valor.length){
         index =0;
