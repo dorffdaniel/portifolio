1b044e503c0fd94dc4ca1b0913ac8a5ad82efcca
Dorffdaniel <danielcontabill@hotmail.com>
2026-01-02 22:04:11 +0000

    alteracao projeto adc ano e idade

--- src/js/script.js
+++ src/js/script.js
@@ -2,18 +2,18 @@ let areaProfissao = document.querySelector('.areaProfissao');
 
 let valor = "Desenvolvedor Front-end";
 
-let index =0;
+let index = 0;
 
-function efeitoPalavra(){
-   let textoAtual = valor.substring(0, index)
+function efeitoPalavra() {
+    let textoAtual = valor.substring(0, index)
 
-   let part1 = textoAtual.substring(0,13)
-   let part2 = textoAtual.substring(13)
-   
-   areaProfissao.innerHTML = `${part1} <span class='amarela'> ${part2}</span>`
+    let part1 = textoAtual.substring(0, 13)
+    let part2 = textoAtual.substring(13)
+
+    areaProfissao.innerHTML = `${part1} <span class='amarela'> ${part2}</span>`
     index++;
-    if(index > valor.length){
-        index =0;
+    if (index > valor.length) {
+        index = 0;
     }
 
 }
@@ -25,7 +25,42 @@ setInterval(efeitoPalavra, 500);
 let hamburguer = document.querySelector('.hamburguer');
 let nav = document.querySelector('.nav-list')
 
-hamburguer.addEventListener('click', ()=>{
+hamburguer.addEventListener('click', () => {
     nav.classList.toggle('ativado')
     hamburguer.classList.toggle('ativa')
 })
+
+function anoAtual() {
+    let ano = new Date().getFullYear();
+
+    return ano;
+}
+
+
+function calcularIdade(dataNasc) {
+
+    const hoje = new Date();
+    const nascimento = new Date(dataNasc);
+
+    let idade = hoje.getFullYear() - nascimento.getFullYear();
+
+    const mesAtual = hoje.getMonth();
+    const mesNascimento = nascimento.getMonth();
+
+    if (mesAtual < mesNascimento || (mesAtual === mesNascimento && hoje.getDate() < nascimento.getDate())
+    ) {
+        idade--;
+    }
+
+    return idade;
+
+}
+
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    document.getElementById("anoAtual").innerHTML = anoAtual();
+    document.getElementById("idade").innerHTML = calcularIdade("1998-10-02");
+
+})
+
